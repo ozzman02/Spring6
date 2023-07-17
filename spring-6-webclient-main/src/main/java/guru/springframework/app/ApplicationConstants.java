7d18c3aaa06c4464852a9a57107f09982422ea0e
osantamaria <osantamaria@gmail.com>
2023-07-16 21:06:47 -0600

    spring web client

---
+++ spring-6-webclient-main/src/main/java/guru/springframework/app/ApplicationConstants.java
@@ -0,0 +1,9 @@
+package guru.springframework.app;
+
+public class ApplicationConstants {
+
+    public static final String BEER_PATH = "/api/v3/beer";
+
+    public static final String BEER_PATH_ID = BEER_PATH + "/{beerId}";
+
+}
---
+++ spring-6-webclient-main/src/main/java/guru/springframework/client/BeerClient.java
@@ -0,0 +1,32 @@
+package guru.springframework.client;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import guru.springframework.model.BeerDTO;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.Map;
+
+public interface BeerClient {
+
+    Flux<String> listBeers();
+
+    Flux<Map> listBeerMap();
+
+    Flux<JsonNode> listBeersJsonNode();
+
+    Flux<BeerDTO> listBeerDtos();
+
+    Mono<BeerDTO> getBeerById(String id);
+
+    Flux<BeerDTO> getBeerByBeerStyle(String beerStyle);
+
+    Mono<BeerDTO> createBeer(BeerDTO beerDTO);
+
+    Mono<BeerDTO> updateBeer(BeerDTO beerDTO);
+
+    Mono<BeerDTO> patchBeer(BeerDTO beerDTO);
+
+    Mono<Void> deleteBeer(BeerDTO dto);
+
+}
