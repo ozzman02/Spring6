3bc2c048fc16e6106b6bef51ae7c1064382353fc
osantamaria <osantamaria@gmail.com>
2023-07-29 00:24:15 -0600

    open api validation with rest assured

--- spring-6-rest-mvc/src/main/java/com/ossant/configuration/SpringSecurityConfig.java
+++ spring-6-rest-mvc/src/main/java/com/ossant/configuration/SpringSecurityConfig.java
@@ -3,12 +3,13 @@ package com.ossant.configuration;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
-
+@Profile("!test")
 @Configuration
 public class SpringSecurityConfig {
 
